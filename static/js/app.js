50a55b18fd6eeb97530646a13d16426ac34400c4
Leonides <leonidesguerra@gmail.com>
2021-05-30 18:48:34 -0500

    plots complete

--- static/js/app.js
+++ static/js/app.js
@@ -15,6 +15,7 @@ let data= d3.json(data_path).then((results)=>{
     let sampleValues=[];
     let otuIds=[];
     let otuLabels=[];
+    
 
     //inserting values on dropdown menu
     s.forEach((v)=>{
@@ -43,11 +44,12 @@ let data= d3.json(data_path).then((results)=>{
         meta.remove();        
         d3.select("#sample-metadata").append("div").attr("class","meta");
         d3.select(".meta").append("p").text(`id: ${subject}`);
-        d3.select(".meta").append("p").text(`id: ${ethnicity}`);
-        d3.select(".meta").append("p").text(`id: ${age}`);
-        d3.select(".meta").append("p").text(`id: ${location}`);
-        d3.select(".meta").append("p").text(`id: ${bbtype}`);
-        d3.select(".meta").append("p").text(`id: ${wfreq}`);
+        d3.select(".meta").append("p").text(`ethnicity: ${ethnicity}`);
+        d3.select(".meta").append("p").text(`Gender: ${gender}`);
+        d3.select(".meta").append("p").text(`Age: ${age}`);
+        d3.select(".meta").append("p").text(`Location: ${location}`);
+        d3.select(".meta").append("p").text(`bbtype: ${bbtype}`);
+        d3.select(".meta").append("p").text(`wfreq: ${wfreq}`);
     
         // Fetching for plot values
         console.log(subject);
@@ -89,18 +91,23 @@ let data= d3.json(data_path).then((results)=>{
         // bar plot
         
         var trace = {
+
             x: values,
             y: otu_id,
             type: "bar",
             orientation:'h',
-            text: labels
+            text: labels,
         };
         var data = [trace];
 
         var layout = {
-            title: "10 most present microbial species",
+            title:{ 
+                text: "10 most present microbial species",
+                font: { size: 24 }
+            },
             xaxis: { title: ""},
-            yaxis: { title: ""}
+            yaxis: { title: ""},
+            font: { color: "darkblue", family: "Arial" },
         };
         
         Plotly.react("bar", data, layout, {displayModeBar: false});
@@ -114,7 +121,8 @@ let data= d3.json(data_path).then((results)=>{
                 size: sampleValues[0],
                 color: otuIds[0],
                 colorscale: 'Portland',
-            }
+            },
+            text: labels,
         };
         var data1 = [trace1];
 
@@ -131,49 +139,63 @@ let data= d3.json(data_path).then((results)=>{
         var trace2 = 
             {
               type: "indicator",
-              mode: "gauge",
-              value: 3,
-              title: { text: "Belly Button Washing Frequency", font: { size: 24 }},
+              mode: "gauge+number",
+              value: wfreq,
+              title: {
+                   text: "Belly Button Washing Frequency<br><span style='font-size:0.8em;color:gray'>scrubs per week</span>",
+                    font: { size: 24 }
+                },
             
-              gauge: {
+                gauge: {
                 
-                axis: { range: [null, 9], tickwidth: 5, tickcolor: "darkblue"},
-                bar: { color: "darkblue", thickness: 0 },
-                bgcolor: "white",
-                borderwidth: 2,
-                bordercolor: "gray",
-                steps:   
-                [
-                  { range: [0, 1 ], color:'#8983'},
-                  { range: [1, 2 ], color:'#8984'},
-                  { range: [2, 3 ], color:'#7975'},
-                  { range: [3, 4 ], color:'#6976'},
-                  { range: [4, 5 ], color:'#5967'},
-                  { range: [5, 6 ], color:'#4968'},
-                  { range: [6, 7 ], color:'#3948'},
-                  { range: [7, 8 ], color:'#2928'},
-                  { range: [8, 9 ], color:'#1919'},
-                   
-                ],
-                
+                    axis: { 
+                        range: [null, 9],
+                        tickmode: 'array',
+                        tickvals:[.5,1.5,2.5,3.5,4.5,5.5,6.4,7.5,8.5], 
+                        ticktext:['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+                        ticks:'',
+                        tickwidth: 5,
+                        tickcolor: "darkblue"
+                    },
+
+                    bar: { color: "darkblue", thickness: 0 },
+                    bgcolor: "white",
+                    borderwidth: 2,
+                    bordercolor: "gray",
+                    steps:   
+                    [
+                    { range: [0, 1 ], color:'#8983'},
+                    { range: [1, 2 ], color:'#8984'},
+                    { range: [2, 3 ], color:'#7975'},
+                    { range: [3, 4 ], color:'#6976'},
+                    { range: [4, 5 ], color:'#5967'},
+                    { range: [5, 6 ], color:'#4968'},
+                    { range: [6, 7 ], color:'#3948'},
+                    { range: [7, 8 ], color:'#2928'},
+                    { range: [8, 9 ], color:'#1919'},
+                    ],
+
+                    threshold:{
+                        line:{color: 'red',width: 5},
+                        value: wfreq,
+                        thickness:.85,
+                        
+                    }
+                }
             }
-                
-
-              }
             
 
-        var data2 =[trace2]
+        var data2 =[trace2];
 
-          var layout = {
+          var layout2 = {
             width: 500,
             height: 400,
-            margin: { t: 25, r: 35, l: 35, b: 25},
-            
-           
+            margin: { t: 25, r: 35, l: 35, b: 5},
             font: { color: "darkblue", family: "Arial" }
           };
+        
           
-         Plotly.newPlot('gauge', data2, layout);
+         Plotly.newPlot('gauge', data2, layout2);
 
 
 
